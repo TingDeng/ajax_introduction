ece5dc22b962cafb1b34ae28a5afa6106b204d67
Ting <tiadeng@gmail.com>
2016-02-18 20:04:51 -0500

    weather218

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-var weatherData = {};//have to be outside of function
+//var weatherData = {};//have to be outside of function
 $(document).ready(function(){
   var baseUrl = 'https://api.forecast.io/forecast/';//make sure this address conrrect for 404 because some websites change their api url
   //var weatherData= null;
   var name ="Your Name";
-  $('#get-weather').on('click', getWeather);
-
+  //$('#get-weather').on('click', getWeather);
+  $('#get-weather').on('click',showInfo);
 
 
   function buildUrl(lat, lon){
@@ -27,12 +27,38 @@ $(document).ready(function(){
 
 
   function successHandler(data){
-    weatherData = data;
+    //weatherData = data;
     $('#output').text(JSON.stringify(data));
     console.log(data);
   }
 
   function errorHandler(err){
     console.log(err);
+  }
+  function showInfo(){
+    var lat=$('#latitude').val();
+    var lon=$('#longitude').val();
+    var ajaxOption = {
+      url: buildUrl(lat,lon),
+      dataType: 'jsonp',
+      success:showInfoSuccess,
+      error: errorHandler
+    };
+    $.ajax(ajaxOption);
+  }
+  function showInfoSuccess(data){
+    console.log(data);//data is an object contains latitude...
+    var source =$('#info').html();
+    var template =Handlebars.compile(source);
+    var extracteddata ={
+      latitude: data.latitude,
+      longitude:data.longitude,
+      icon:data.currently.icon ||"clear-night",
+      summary:data.currently.summary,
+      time:moment(data.currently.time).format('MMMM Do YYYY, h:mm:ss a')
+    };
+    //data is from server
+    var html = template(extracteddata);
+    $('#test-output').html(html);
   }
 });
